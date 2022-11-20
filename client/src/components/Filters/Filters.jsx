842ac330708075c29ded7e792fb8567866c62c9f
Joaquin Delgado <jotadelgado96@gmail.com>
2022-11-20 19:00:23 -0300

    commit

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -3,12 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { creationFilter, getAllTemperaments, orderFilter, orderWeightFilter, setCurrentPage, temperamentsFilter } from "../../actions";
 import './Filter.css';
 
-export default function Filters(){
+export default function Filters() {
 
     const dispatch = useDispatch();
     const allTemperaments = useSelector(state => state.temperaments)
 
-    const handleOrderFilter= e =>{
+    const handleOrderFilter = e => {
         dispatch(orderFilter(e.target.value));
     }
 
@@ -30,35 +30,43 @@ export default function Filters(){
         dispatch(getAllTemperaments())
     }, [dispatch]);
 
-    return(
+    return (
         <div className="cont">
             <div className="filter">
-                <select defaultValue={"Alphabetic Order..."} onChange={(e) => handleOrderFilter(e)}>
-                    <option disabled>Alphabetic Order...</option>
-                    <option>A - Z</option>
-                    <option>Z - A</option>
-                </select>
+                <div className="selects">
+                    <select defaultValue={"Alphabetic Order..."} onChange={(e) => handleOrderFilter(e)}>
+                        <option disabled>Alphabetic Order...</option>
+                        <option>A - Z</option>
+                        <option>Z - A</option>
+                    </select>
+                </div>
 
-                <select defaultValue={"Weight..."} onChange={(e) => handleWeightFilter(e)}>
-                    <option disabled>Weight...</option>
-                    <option>Less</option>
-                    <option>More</option>
-                </select>
+                <div className="selects">
+                    <select defaultValue={"Weight..."} onChange={(e) => handleWeightFilter(e)}>
+                        <option disabled>Weight...</option>
+                        <option>Less</option>
+                        <option>More</option>
+                    </select>
+                </div>
 
-                <select onChange={(e) => createdBreeds(e)}>
-                    <option>All Breeds</option>
-                    <option>Created</option>
-                    <option>Existing</option>
-                </select>
+                <div className="selects">
+                    <select onChange={(e) => createdBreeds(e)}>
+                        <option>All Breeds</option>
+                        <option>Created</option>
+                        <option>Existing</option>
+                    </select>
+                </div>
 
-                <select onChange={e => handleTemperamentsFilter(e)}>
-                    <option>All Temperaments</option>
-                    {allTemperaments?.map(t => {
-                        return (
-                            <option key={t.name} value={t.name}>{t.name}</option>
-                        )
-                    })}
-                </select>
+                <div className="selects">
+                    <select onChange={e => handleTemperamentsFilter(e)}>
+                        <option>All Temperaments</option>
+                        {allTemperaments?.map(t => {
+                            return (
+                                <option key={t.name} value={t.name}>{t.name}</option>
+                            )
+                        })}
+                    </select>
+                </div>
             </div>
         </div>
     )
--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./Pagination.css";
 
 export default function Pagination({perPage, totalBreeds, paginate, previousPaginate, nextPaginate}) {
 
     const pageNumbers = [];
+    const active = useSelector(state => state.page);
 
     for(let i=1 ; i <= Math.ceil(totalBreeds/perPage); i++){
         pageNumbers.push(i);
@@ -16,7 +18,7 @@ export default function Pagination({perPage, totalBreeds, paginate, previousPagi
                     <button className="btt" onClick={() => previousPaginate()}>{"<<"}</button>
 
                     {pageNumbers.map(n =>(
-                        <button key={n} className="btt" onClick={() => paginate(n)}>{n}</button>    
+                        <button key={n} className={active===n ? "active" : "btt"} onClick={() => paginate(n)}>{n}</button>    
                         )
                     )}
                     <button className="btt" onClick={() => nextPaginate()}>{">>"}</button>
