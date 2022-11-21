dd673c06dd7c074580f9b0773498ff64d8e7fe57
Joaquin Delgado <jotadelgado96@gmail.com>
2022-11-21 20:35:14 -0300

    commit

--- client/src/components/CreateBreed/CreateBreed.jsx
+++ client/src/components/CreateBreed/CreateBreed.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }  from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { createBreed, getAllTemperaments } from "../../actions";
 import { useHistory, Link } from "react-router-dom";
@@ -6,29 +6,29 @@ import './CreateBreed.css';
 
 function validate(breed) {
     let errors = {};
-    if(!breed.name || breed.name.length < 3){
+    if (!breed.name || breed.name.length < 3) {
         errors.name = "Name to short";
     }
-    if (breed.height_min < 1 || breed.height_min > 100){
+    if (breed.height_min < 1 || breed.height_min > 100) {
         errors.height_min = "Height min must be between 1 - 100";
     }
-    if (breed.height_max < 1 || breed.height_max > 100){
+    if (breed.height_max < 1 || breed.height_max > 100) {
         errors.height_max = "Height max must be between 1 - 100";
     }
-    if (breed.weight_min < 1 || breed.weight_min > 100){
+    if (breed.weight_min < 1 || breed.weight_min > 100) {
         errors.weight_min = "Weight min must be between 1 - 100";
     }
-    if (breed.weight_max < 1 || breed.weight_max > 100){
+    if (breed.weight_max < 1 || breed.weight_max > 100) {
         errors.weight_max = "Weight max must be between 1 - 100";
     }
-    if (breed.life_span < 1 || breed.life_span > 25){
+    if (breed.life_span < 1 || breed.life_span > 25) {
         errors.life_span = "Life span must be between 1 - 25";
     }
-    if (breed.height_min > breed.height_max){
+    if (breed.height_min > breed.height_max) {
         errors.height_min = "Height min can't be higher than max"
         errors.height_max = "Height max can't be lower than min"
     }
-    if (breed.weight_min > breed.weight_max){
+    if (breed.weight_min > breed.weight_max) {
         errors.weight_min = "Weight min can't be higher than max"
         errors.weight_max = "Weight max can't be lower than min"
     }
@@ -57,14 +57,14 @@ export default function CreateBreed() {
 
 
     let disabled =
-    !(
-        breed.name.length &&
-        breed.height_min.length &&
-        breed.height_max.length &&
-        breed.weight_min.length &&
-        breed.weight_max.length &&
-        breed.temperament.length
-    ) ||
+        !(
+            breed.name.length &&
+            breed.height_min.length &&
+            breed.height_max.length &&
+            breed.weight_min.length &&
+            breed.weight_max.length &&
+            breed.temperament.length
+        ) ||
         breed.height_min > 100 || breed.height_min < 1 ||
         breed.height_max > 100 || breed.height_max < 1 ||
         breed.weight_min > 100 || breed.weight_min < 1 ||
@@ -78,11 +78,11 @@ export default function CreateBreed() {
     }, [dispatch])
 
     const handleChange = e => {
-            setBreed({
-                ...breed,
-                [e.target.name]: e.target.value
-            })
-        
+        setBreed({
+            ...breed,
+            [e.target.name]: e.target.value
+        })
+
         setErrors(validate({
             ...breed,
             [e.target.name]: e.target.value
@@ -105,123 +105,143 @@ export default function CreateBreed() {
 
 
     return (
-        <div>
-            <div>
-                <Link to={`/dogs`}>
-                    <button>Back</button>
-                </Link>
-            </div>
-            <h3>Create your Breed</h3>
-            <form onSubmit={e => handleSubmit(e)} autoComplete="off">
-                
-                <label>Enter name:
-                    <input 
-                        onChange={(e) => handleChange(e)}
-                        type="text"
-                        name="name"
-                        value={breed.name}
-                        placeholder="Enter name..."
-                    />
-                    {errors.name && <p className="errors">{errors.name}</p>}
-                </label>
-                
-                <label>Height Min:
-                    <input
-                        onChange={(e) => handleChange(e)}
-                        type="number"
-                        name="height_min"
-                        value={breed.height_min}
-                        placeholder="1 - 100..."
-                    />
-                    {errors.height_min && <p className="errors">{errors.height_min}</p>}
-                </label>
-                
-                <label>Height Max:
-                    <input 
-                        onChange={(e) => handleChange(e)}
-                        type="number"
-                        name="height_max"
-                        value={breed.height_max}
-                        placeholder="1 - 100..."
-                    />
-                    {errors.height_max && <p className="errors">{errors.height_max}</p>}
-                </label>
-
-                <label>Weight Min:
-                    <input
-                        onChange={(e) => handleChange(e)}
-                        type="number"
-                        name="weight_min"
-                        value={breed.weight_min}
-                        placeholder="1 - 100..."
-                    />
-                    {errors.weight_min && <p className="errors">{errors.weight_min}</p>}
-                </label>
-
-                <label>Weight Max:
-                    <input
-                        onChange={(e) => handleChange(e)}
-                        type="number"
-                        name="weight_max"
-                        value={breed.weight_max}
-                        placeholder="1 - 100..."
-                    />
-                    {errors.weight_max && <p className="errors">{errors.weight_max}</p>}
-                </label>
-
-                <label>Life span:
-                    <input
-                        onChange={(e) => handleChange(e)}
-                        type="number"
-                        name="life_span"
-                        value={breed.life_span}
-                        placeholder="1 - 25..."
-                    />
-                    {errors.life_span && <p className="errors">{errors.life_span}</p>}
-                </label>
-                
-                <label>Image:
-                    <input 
-                        onChange={(e) => handleChange(e)}
-                        type="url"
-                        name="img"
-                        value={breed.img}
-                        placeholder="URL..."
-                    />
-                </label>
-
-                <div>
-                    <select
-                        onChange={(e) => handleSelect(e)}
-                        value="Temperaments"
-                    >
-                        <option disabled>Temperaments</option>
-                        {temperaments?.map(t => {
-                            return (
-                                <option
-                                    value={t.name}
-                                    key={t.id}
-                                >
-                                    {t.name}
-                                </option>
-                            )
-                        })}
-                    </select>
-
-                    <ul>
-                        {breed.temperament?.map(t => {
-                            return (
-                                <li key={t}>
-                                    {t}
-                                </li>
-                            )
-                        })}
-                    </ul>
-
+        <div className="createcontainer">
+            <div className="createback">
+                <div className="prueba">
+                    <Link to={`/dogs`}>
+                        <button className="buttoncreate">Back</button>
+                    </Link>
                 </div>
+                <div className="createtitle">
+                    <h3>Create your Breed</h3>
+                </div>
+            </div>
+            <div className="formcontainer">
+                <form onSubmit={e => handleSubmit(e)} autoComplete="off">
+                    <div className="formdiv">
+                        <label className="labelform">Enter name:
+                            <input
+                                onChange={(e) => handleChange(e)}
+                                className="inputform"
+                                type="text"
+                                name="name"
+                                value={breed.name}
+                                placeholder="Name..."
+                            />
+                            {errors.name && <p className="errors">{errors.name}</p>}
+                        </label>
+                    </div>
+                    <div className="formdiv">
+                        <label className="labelform">Height Min:
+                            <input
+                                onChange={(e) => handleChange(e)}
+                                className="inputform"
+                                type="number"
+                                name="height_min"
+                                value={breed.height_min}
+                                placeholder="1 - 100..."
+                            />
+                            {errors.height_min && <p className="errors">{errors.height_min}</p>}
+                        </label>
+                    </div>
+                    <div className="formdiv">
+                        <label className="labelform">Height Max:
+                            <input
+                                onChange={(e) => handleChange(e)}
+                                className="inputform"
+                                type="number"
+                                name="height_max"
+                                value={breed.height_max}
+                                placeholder="1 - 100..."
+                            />
+                            {errors.height_max && <p className="errors">{errors.height_max}</p>}
+                        </label>
+                    </div>
+                    <div className="formdiv">
+                        <label className="labelform">Weight Min:
+                            <input
+                                onChange={(e) => handleChange(e)}
+                                className="inputform"
+                                type="number"
+                                name="weight_min"
+                                value={breed.weight_min}
+                                placeholder="1 - 100..."
+                            />
+                            {errors.weight_min && <p className="errors">{errors.weight_min}</p>}
+                        </label>
+                    </div>
+                    <div className="formdiv">
+                        <label className="labelform">Weight Max:
+                            <input
+                                onChange={(e) => handleChange(e)}
+                                className="inputform"
+                                type="number"
+                                name="weight_max"
+                                value={breed.weight_max}
+                                placeholder="1 - 100..."
+                            />
+                            {errors.weight_max && <p className="errors">{errors.weight_max}</p>}
+                        </label>
+                    </div>
+                    <div className="formdiv">
+                        <label className="labelform">Life span:
+                            <input
+                                onChange={(e) => handleChange(e)}
+                                className="inputform"
+                                type="number"
+                                name="life_span"
+                                value={breed.life_span}
+                                placeholder="1 - 25..."
+                            />
+                            {errors.life_span && <p className="errors">{errors.life_span}</p>}
+                        </label>
+                    </div>
+                    <div className="formdiv">
+                        <label className="labelform">Image:
+                            <input
+                                onChange={(e) => handleChange(e)}
+                                className="inputform"
+                                type="url"
+                                name="img"
+                                value={breed.img}
+                                placeholder="URL..."
+                            />
+                        </label>
+                    </div>
+                    <div className="tempforms">
+                        <select
+                            onChange={(e) => handleSelect(e)}
+                            value="Temperaments"
+                        >
+                            <option disabled>Temperaments</option>
+                            {temperaments?.map(t => {
+                                return (
+                                    <option
+                                        value={t.name}
+                                        key={t.id}
+                                    >
+                                        {t.name}
+                                    </option>
+                                )
+                            })}
+                        </select>
 
-                <button disabled={disabled} type="submit">Create Breed!!</button>
-            </form>
+                        <ul>
+                            {breed.temperament?.map(t => {
+                                return (
+                                    <li key={t}>
+                                        {t}
+                                    </li>
+                                )
+                            })}
+                        </ul>
+
+                    </div>
+                    <div className="divbtnform">
+                        <button className="btnform" disabled={disabled} type="submit">Create Breed!!</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 
