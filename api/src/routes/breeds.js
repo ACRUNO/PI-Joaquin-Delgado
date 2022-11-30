e56c515c051b3d41ab7f7f470a595d0879a72fee
Joaquin Delgado <jotadelgado96@gmail.com>
2022-11-30 14:17:04 -0300

    commit

--- api/src/routes/breeds.js
+++ api/src/routes/breeds.js
@@ -4,25 +4,25 @@ const router = Router();
 const { getBreedByName, getBreedId, getAllBreeds } = require('../Controller/functions.js');
 
 
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
     try {
         const { name } = req.query;
         const breeds = await getAllBreeds();
 
-        if (name){
+        if (name) {
             const breedsFiltered = getBreedByName(name, breeds);
             return res.status(200).send(breedsFiltered);
         }
-            
-            return res.status(200).send(breeds);
-        
-        
+
+        return res.status(200).send(breeds);
+
+
     } catch (error) {
         res.status(400).send(error.message);
     }
 })
 
-router.get('/:id', async(req, res) => {
+router.get('/:id', async (req, res) => {
     try {
         const { id } = req.params;
         const breeds = await getAllBreeds();
@@ -38,7 +38,7 @@ router.post('/', async (req, res) => {
     try {
         let { id, name, height_min, height_max, weight_min, weight_max, life_span, img, temperament } = req.body;
         if (!name || !height_min || !height_max || !weight_min || !weight_max) throw new Error('Missing data!');
-        if(!img) img = 'https://img.europapress.es/fotoweb/fotonoticia_20170622121827-17062170119_1200.jpg';
+        if (!img) img = 'https://img.europapress.es/fotoweb/fotonoticia_20170622121827-17062170119_1200.jpg';
         const newBreed = await Breed.create({
             id,
             name,
@@ -46,7 +46,7 @@ router.post('/', async (req, res) => {
             height_max,
             weight_min,
             weight_max,
-            life_span, 
+            life_span,
             img
         })
 
@@ -58,13 +58,28 @@ router.post('/', async (req, res) => {
             });
             newBreed.addTemperament(newTemperament);
         })
-        
+
         res.status(201).send(newBreed);
     } catch (error) {
         res.status(400).send(error.message);
     }
 })
 
+
+router.delete('/:id', async (req, res) => {
+    try {
+        let { id } = req.params;
+        await Breed.destroy({
+            where: {
+                id: id
+            }
+        });
+        console.log(id);
+        res.status(201).send(id);
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
+})
 
 
 module.exports = router;
